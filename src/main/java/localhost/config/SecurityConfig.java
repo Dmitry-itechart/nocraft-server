1d91a6c013855dcf3958245c7a0701e5ffde4f14
Dmitry.Prakapenka <dima.prokopenko@gmail.com>
2020-01-29 16:49:34 +0300

    - temporary allow localhost:4200

--- src/main/java/localhost/config/SecurityConfig.java
+++ src/main/java/localhost/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
@@ -34,7 +36,17 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest()
-                .permitAll()
+                .permitAll().and().cors().disable().authorizeRequests();
         ;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
